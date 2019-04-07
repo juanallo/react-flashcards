47fbcbf0e018b9025895a72173999950f2080ba4
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2019-04-06 23:27:40 -0700

    adding style to navigation elements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -34,9 +34,22 @@ const Button = styled.button`
     background: #0000;
     width: 100%;
     text-align: left;
-    padding: 10px 20px;
+    padding: 10px 20px 10px 8px;
     margin-bottom: 5px;
     font-size: 16px;
+    border-left: 6px solid transparent;
+    box-sizing: border-box;
+    
+    &.selected {
+    	border-left: 6px solid #5352ed;
+    	background: #a4b0be;
+    }
+    
+    :hover {
+    	opacity: 0.6;
+    	border-left: 6px solid #5352ed;
+    	background: #a4b0be;
+    }
 `;
 
 
@@ -48,9 +61,14 @@ const Navigation = (props) => {
 				<Title>Flash Cards</Title>
 			</Header>
 			<Ul>
-				{props.items.map(item => (
+				{props.items.map((item, i) => (
 					<li key={item.id}>
-						<Button tabIndex={props.show ? 0 : -1} onClick={e => props.onSelection(e, item.id)}>{item.label}</Button>
+						<Button className={i === 0 ? 'selected' : ''}
+						        tabIndex={props.show ? 0 : -1}
+						        onClick={e => props.onSelection(e, item.id)}
+						>
+							{item.label}
+						</Button>
 					</li>
 				))}
 			</Ul>
